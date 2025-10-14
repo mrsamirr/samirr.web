9d9861d25555f6c29532833897d1bf2d4b3bf2a7
Md Samer Ansari <115770126+mrsamirr@users.noreply.github.com>
2025-10-14 12:56:24 +0530

    update experience section

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -1,12 +1,12 @@
-  export const experiences = [
-    {
-      company: "GrowthPurple Technologies",
-      position: "Software Engineer Intern",
-      duration: "Jun 2025 - Present",
-    },
-    {
-      company: "Kureita",
-      position: "Frontend Development Intern",
-      duration: "October 2024 - December 2024",
-    },
-  ]
+export const experiences = [
+  {
+    company: "GrowthPurple Technologies",
+    position: "Software Engineer Intern",
+    duration: "Jun 2025 - October 2025",
+  },
+  {
+    company: "Kureita",
+    position: "Frontend Development Intern",
+    duration: "October 2024 - December 2024",
+  },
+];
